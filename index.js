b17176edbcae96b9448c4667f2c01d8805685288
alexander0ezhov <alexander0ezhov@hotmail.com>
2020-01-21 03:05:05 +0300

    методы изменены на асинхронные, так же исправлены баги и вылеты при неправильных именах файлов

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
-const Clipper = require('image-clipper');
+const imageClipper = require('image-clipper');
 const fs = require("fs");
 //Пути к изображениям, json файлам и путь вывода.
-
-const sourcePath =  '/home/ezhov/Рабочий стол/ztest/src';
-const projectPath = '/home/ezhov/Рабочий стол/ztest/project';
-const outputPath =  '/home/ezhov/Рабочий стол/ztest/output';
-
-/*
-const sourcePath = 'C:\\Users\\alexa\\Desktop\\VottSnils\\sourceImages';
-const projectPath = 'C:\\Users\\alexa\\Desktop\\VottSnils\\Project';
-const outputPath =  'C:\\Users\\alexa\\Desktop\\ztest\\output';
-*/
-
+const {sourcePath, projectPath, outputPath} = require('./path.json');
 const Canvas = require('canvas');
-Clipper.configure('canvas',Canvas);
+imageClipper.configure('canvas',Canvas);
 
-let tFile,height,width,x, y,outputFile;
-
-fs.readdirSync(projectPath).forEach(filename=>{
-    if (filename.indexOf('.json') > 0) {
-        tFile = require(projectPath + '/' + filename);
-        tFile.regions.forEach(region=> {
-            if (region.type === 'RECTANGLE') {
-                height = region.boundingBox.height;
-                width = region.boundingBox.width;
-                x = region.points[0].x;
-                y = region.points[0].y;
-                outputFile = outputPath + '/' + region.id + tFile.asset.name;
-                Clipper(sourcePath + '/' + tFile.asset.name, function () {
-                    this.crop(x, y, width, height)
-                        .toFile(outputFile, function () {
-                            console.log('saved!');
-                        });
-                });
+async function start() {
+    let prj = fs.readdirSync(projectPath);
+    let conf = {};
+        for (let filename of prj) {
+            if (filename.indexOf('.json') > 0) {
+                conf.tFile = require(projectPath + '/' + filename);
+                for (let region of conf.tFile.regions) {
+                    if (region.type === 'RECTANGLE') {
+                        conf.height = region.boundingBox.height;
+                        conf.width = region.boundingBox.width;
+                        conf.x = region.points[0].x;
+                        conf.y = region.points[0].y;
+                        conf.outputFile = outputPath + '/' + region.id +region.tags[0]+ decodeURI(conf.tFile.asset.name);
+                        console.log('in: ',filename);
+                        await write(conf).then(result=>console.log(result)) .catch((err)=>{console.log(err)});
+                    }
+                }
             }
-        });
-    }
-});
+        }
+}
+
+function write(conf) {
+    return new Promise((resolve,reject)=>{
+         console.log('out:', conf.outputFile);
+         //временно пропускаем файлы с кириллицей
+        if (!decodeURI(conf.tFile.asset.name).match(/[а-яА-Я]/g)) {
+            imageClipper(sourcePath + '/' + decodeURI(conf.tFile.asset.name), function () {
+                this.crop(conf.x, conf.y, conf.width, conf.height)
+                    .toFile(conf.outputFile, function () {
+                        resolve('saved: '+conf.outputFile);
+                        reject('error');
+                    });
+            });
+        } else {
+            resolve('skipped')
+        }
+    })
+}
+
+start();
