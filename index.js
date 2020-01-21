c3cba99979d9845101b50b6e18b666bd28172587
alexander0ezhov <alexander0ezhov@hotmail.com>
2020-01-21 12:02:46 +0300

    3

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const imageClipper = require('image-clipper');
 const fs = require("fs");
 //Пути к изображениям, json файлам и путь вывода.
-const {sourcePath, projectPath, outputPath} = require('./path.json');
+const {projectPath, output} = require('./path.json');
 const Canvas = require('canvas');
 imageClipper.configure('canvas',Canvas);
+
+const sourcePath = "/home/ezhov/Рабочий стол/tst/VOTT/sourceImages";
 
 async function start() {
     let prj = fs.readdirSync(projectPath);
@@ -17,9 +19,13 @@ async function start() {
                         conf.width = region.boundingBox.width;
                         conf.x = region.points[0].x;
                         conf.y = region.points[0].y;
-                        conf.outputFile = outputPath + '/' + region.id +region.tags[0]+ decodeURI(conf.tFile.asset.name);
-                        console.log('in: ',filename);
-                        await write(conf).then(result=>console.log(result)) .catch((err)=>{console.log(err)});
+                        for (let el of output) {
+                            if (el.tags.includes(region.tags[0])) {
+                                console.log('in: ', filename);
+                                conf.outputFile = `${el.path}/${create_UUID()}.jpg`;
+                                await write(conf).then(result=>console.log(result)) .catch((err)=>{console.log(err)});
+                            }
+                        }
                     }
                 }
             }
@@ -30,7 +36,8 @@ function write(conf) {
     return new Promise((resolve,reject)=>{
          console.log('out:', conf.outputFile);
          //временно пропускаем файлы с кириллицей
-        if (!decodeURI(conf.tFile.asset.name).match(/[а-яА-Я]/g)) {
+        // if (!decodeURI(conf.tFile.asset.name).match(/[а-яА-Я]/g)) {
+            // let sourcePath = conf.tFile.asset.path.slice(5, -conf.tFile.asset.name.length-1);
             imageClipper(sourcePath + '/' + decodeURI(conf.tFile.asset.name), function () {
                 this.crop(conf.x, conf.y, conf.width, conf.height)
                     .toFile(conf.outputFile, function () {
@@ -38,10 +45,20 @@ function write(conf) {
                         reject('error');
                     });
             });
-        } else {
-            resolve('skipped')
-        }
+        // } else {
+        //     resolve('skipped')
+        // }
     })
+}
+
+function create_UUID(){
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
 }
 
 start();
